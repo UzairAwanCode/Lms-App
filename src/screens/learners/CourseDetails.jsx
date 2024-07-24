841272221feddd5fb054e79d3b53b61f9c18d46b
UzairAwanCode <githubprojects000@gmail.com>
2024-07-25 00:59:56 +0500

    Make new folder courses inside learner and make file CourseDetails.jsx

---
+++ src/screens/learners/CourseDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CourseDetails = () => {
+  return (
+    <View>
+      <Text>Course Details</Text>
+    </View>
+  );
+}
+
+export default CourseDetails;
